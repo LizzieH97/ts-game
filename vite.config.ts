1eeb63a3db8fcd55929ddea019744c0159d017d5
LizzieH97 <erhughes97@gmail.com#>
2025-04-17 21:09:11 +0100

    STILL trying to deploy, changed ts and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/ts-game/", // <-- important!
+  build: {
+    outDir: "dist", // Make sure output is going to the `dist` directory
+  },
 });
